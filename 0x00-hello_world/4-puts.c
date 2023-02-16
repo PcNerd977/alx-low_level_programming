cf70905731f57f9b780207547b4059fb07e671b6
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-16 17:23:19 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-   /*I want to write the program that print "Programming is like 
-    *building a multilingual puzzle
-    */
+/**
+*I want to write the program that print "Programming is like
+*building a multilingual puzzle
+*/
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return(0);
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
