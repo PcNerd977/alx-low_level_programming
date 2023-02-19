6d5017159e850f2b0ef45aeebc162e912a197aed
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-19 01:48:47 +0100

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print all the alphabet in lower case
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int n;
+
+for (n = 97; n <= 122; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
