9a714a187bc8a28be653a65f51a5a1c058ab2657
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-19 10:40:18 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - this program print all the combination of single digit
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+n = 48;
+while (n < 58)
+{
+putchar(n);
+putchar(',');
+putchar(' ');
+n += 1;
+}
+return (0);
+}
