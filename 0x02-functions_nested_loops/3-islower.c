fa40a88770d55686b0bc09bc4359cc309c05b3d5
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-22 10:32:08 +0100

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check if an alphabet is lower or uppercase
+ *
+ * c -  takes in the alphabet to check
+ * Return: 1 is successful
+ */
+
+int _islower(int c)
+{;
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
