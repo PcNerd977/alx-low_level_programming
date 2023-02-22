f89fc574387ea4487d58da85c0730c5e79006d7e
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-22 11:35:54 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - check if a number is positive, negative or zero
+ *
+ * Return: -1 if negative, +1 if positve or 0 if zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
