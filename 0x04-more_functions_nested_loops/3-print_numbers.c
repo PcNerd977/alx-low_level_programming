028b8197d110986fc5087b08b2eeef5278ba968c
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-23 21:12:40 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print all the numbers from 0 to 9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	for (int a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
