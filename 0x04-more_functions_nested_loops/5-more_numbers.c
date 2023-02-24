4c00d6a39141307a06937387b312c7b63ef0e7e1
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-24 01:49:52 +0100

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,19 @@ void more_numbers(void)
 	while (a <= 10)
 	{
 		int b;
+		int c;
 
+		c = 48;
 		for (b = 48; b <= 57; b++)
+		{
 			_putchar(b);
+		}
+		while (c <= 52)
+		{
+			_putchar('1');
+			_putchar(c);
+			c += 1;
+		}
 		_putchar('\n');
 		a += 1;
 	}
