dca6f001db3c2120e671ce4b8f8d600e58f190df
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-24 01:11:05 +0100

    correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,21 @@ int main(void)
 	{
 		if ((a % 3) == 0 && (a % 5) == 0)
 			printf("FizzBuzz ");
-		
+
 		else if ((a % 3) == 0)
 			printf("Fizz ");
 
 		else if ((a % 5) == 0)
-			printf("Buzz ");
+			printf("Buzz");
 
 		else
-			printf("%d ", a);
+			printf("%d", a);
+		if (a == 100)
+			;
+		else
+			putchar(' ');
 
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
