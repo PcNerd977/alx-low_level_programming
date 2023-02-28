302e9feb7c20cd2dffd43353436ef0ea1862a2a1
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-28 11:32:05 +0100

    ammendments

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,9 @@
+/**
+ * reset_to_98 - change the value of a variable
+ *
+ * @n: it is a pointer
+ * Return: nothing
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,10 @@
+/**
+ * swap_int - this function swap the value contained in variable
+ * @a: pointer containing the address of a variable
+ * @b: pointer containing the address of a variable
+ * Return: nothing
+ */
+
 void swap_int(int *a, int *b)
 {
 	int tmp;
@@ -5,5 +12,4 @@ void swap_int(int *a, int *b)
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
-	
 }
