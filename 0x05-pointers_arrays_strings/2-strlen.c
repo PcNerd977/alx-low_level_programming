fcabeb2d3d0a0e877afc3e37cd2a08e5694a1d92
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-02-28 11:41:38 +0100

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,19 @@
+/**
+ * _strlen - retun the lenght of a string
+ * @s: a pointer 
+ * Return: lenght
+ */
 int _strlen(char *s)
 {
 	int lenght;
+	int i;
 
-	while (s != 0)
+	lenght = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
 		lenght += 1;
+		i++;
+	}
 	return (lenght);
 }
