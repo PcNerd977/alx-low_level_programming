8777f5e237823c00dc8e7ce818487fc726d6a3f0
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-03-03 10:40:20 +0100

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char *rot13(char *str)
+{
+	int i;
+	int end_loop = 0;
+	int j;
+	char list[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char map[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		end_loop = 0;
+		for (j = 0; j < 52 && end_loop == 0; j++)
+		{
+			if (str[i] == list[j])
+			{
+				str[i] = map[j];
+				end_loop = 1;
+			}
+		}
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,3 +7,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char);
