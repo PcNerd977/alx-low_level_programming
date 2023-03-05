e029da73fc87f0f77151053d09a8ce815f033fc6
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-03-05 12:11:32 +0100

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int a;
+	int result = 0;
+	int end_loop = 0;
+	int sign = 1;
+	int if_number = 0;
+
+	for (a = 0; s[a] != '\0' && end_loop == 0; a++)
+	{
+		if (s[a] == '-')
+			sign = -1 * sign;
+		else if (s[a] >= '0' && s[a] <= '9')
+		{
+			result = result * 10 + (s[a] - '0');
+			if_number = 1;
+		}
+		if (if_number > 0)
+		{
+			if (s[a + 1] >= '0' && s[a + 1] <= '9')
+				continue;
+			else
+				end_loop = 1;
+		}	
+	}
+	result = sign * result;
+	if (result == 0)
+		return (0);
+	else
+		return (result);
+}
