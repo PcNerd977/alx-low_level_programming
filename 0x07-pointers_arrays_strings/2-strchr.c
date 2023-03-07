0693768c3bf2a61ff390513bae08341f192faf4a
Ajayi Habeeb <ajayihabeeb977@gmail.com>
2023-03-07 03:50:26 +0100

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - search for a character in a string
+ * @s: a pointer
+ * @c: a character
+ * Return: a pointer
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	int end_loop = 0;
+	int char_found = 0;
+	char *ptr;
+
+	for (i = 0; s[i] != '\0' && end_loop == 0; i++)
+	{
+		if (s[i] == c)
+			end_loop = 1;
+	}
+	ptr = s + i - 1;
+	if (end_loop != 0)
+		return (ptr);
+	else
+		return ('\0');
+}
